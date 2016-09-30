de10a94a1e96256d813e0958ef9aaa010727fabf
Andrew <andrew.vinocur@gmail.com>
2016-09-30 14:44:09 -0700

    completes edit/update functionality steps

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -8,7 +8,7 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
+    # creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creatures_path(creature)
@@ -20,4 +20,21 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
   end
 
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 end
