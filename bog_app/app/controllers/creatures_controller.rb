7523bf0f8e62f7db4869612b75ccd40ea14ec7aa
Andrew <andrew.vinocur@gmail.com>
2016-09-30 15:54:06 -0700

    adds working validation in model. Need to implement flash message for errors

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -9,9 +9,11 @@ class CreaturesController < ApplicationController
 
   def create
     # creature_params = params.require(:creature).permit(:name, :description)
-    creature = Creature.new(creature_params)
-    if creature.save
-      redirect_to creatures_path(creature)
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creature_path(@creature)
+    else
+      render :new
     end
   end
 
--- bog_app/app/models/creature.rb
+++ bog_app/app/models/creature.rb
@@ -1,2 +1,4 @@
 class Creature < ApplicationRecord
+  validates_presence_of :name, acceptance: true, message: 'You must name your Bog Creature'
+  validates_presence_of :description, acceptance: true, message: 'You must describe your Bog Creature'
 end
