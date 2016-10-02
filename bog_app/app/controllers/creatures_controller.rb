41cfe7f19dd5a941fb39e3180fbf60967ef0d69b
Andrew <andrew.vinocur@gmail.com>
2016-10-02 11:25:36 -0700

    completes part 3

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -14,11 +14,28 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
     end
   end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 
 end
